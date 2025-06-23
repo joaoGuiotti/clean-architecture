bb38d227b295737e0eada29de30d17d14b36b950
joaoGuiotti <joao.guiotti012@gmail.com>
2025-06-23 14:02:02 -0300

    feat: enhance product validation with error notifications and improve unit tests

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,7 +1,6 @@
 import Product from "./product";
 
 describe('Product unit tests', () => {
-
     it('Should throw error when id is empty', () => {
         expect(() => {
             const product = new Product('', 'Product 1', 100.20);
@@ -14,10 +13,10 @@ describe('Product unit tests', () => {
         }).toThrow('Name is required');
     });
 
-    it('should throw error when price  is less than zero', () => {
+    it('should throw error when price is less than zero', () => {
         expect(() => {
             const prod = new Product('12', 'Prod 1', -1);
-        }).toThrow('Price must be greater than zero');
+        }).toThrow('Price must be greater than or equal to zero');
     });
 
     it('should change name', () => {
@@ -25,9 +24,30 @@ describe('Product unit tests', () => {
         prod.changeName('Prod 2');
         expect(prod.name).toBe('Prod 2');
     });
+
+    it('should throw error when changing name to empty', () => {
+        const prod = new Product('12', 'Prod 1', 1);
+        expect(() => {
+            prod.changeName('');
+        }).toThrow('Name is required');
+    });
+
     it('should change price', () => {
         const prod = new Product('12', 'Prod 1', 1);
         prod.changePrice(99.99);
         expect(prod.price).toBe(99.99);
+    });
+
+    it('should throw error when changing price to less than zero', () => {
+        const prod = new Product('12', 'Prod 1', 1);
+        expect(() => {
+            prod.changePrice(-1);
+        }).toThrow('Price must be greater than or equal to zero');
+    });
+
+    it('should get more than one error', () => {
+        expect(() => {
+            new Product('', '', -1);
+        }).toThrow('Product: Id is required, Product: Name is required, Product: Price must be greater than or equal to zero');
     });
 })
--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,19 +1,17 @@
+import Entity from "../../shared/entity/entity.abstract";
+import NotificationError from "../../shared/notification/notification.error";
 import IProduct from "./product.interface";
 
-export default class Product implements IProduct {
-    private _id: string;
+export default class Product extends Entity implements IProduct {
     private _name: string;
     private _price: number = 0;
 
     constructor(id: string, name: string, price: number) {
+        super();
         this._id = id;
         this._name = name;
         this._price = price;
-        this.validate();
-    }
-
-    get id() {
-        return this._id;
+        this.validateErrors();
     }
 
     get name() {
@@ -26,25 +24,33 @@ export default class Product implements IProduct {
 
     changeName(name: string) {
         this._name = name;
-        this.validate();
+        this.validateErrors();
     }
 
     changePrice(price: number) {
         this._price = price;
-        this.validate();
+        this.validateErrors();
     }
 
     validate() {
         if (this._id.length === 0) {
-            throw new Error('Id is required');
+            this.notification.addError({
+                context: 'Product',
+                message: 'Id is required'
+            });
         }
         if (this._name.length === 0) {
-            throw new Error('Name is required');
+            this.notification.addError({
+                context: 'Product',
+                message: 'Name is required'
+            });
         }
         if (this._price < 0) {
-            throw new Error('Price must be greater than zero');
+            this.notification.addError({
+                context: 'Product',
+                message: 'Price must be greater than or equal to zero'
+            });
         }
-        return true;
     }
 
     toJSON() {
@@ -52,6 +58,13 @@ export default class Product implements IProduct {
             id: this._id,
             name: this._name,
             price: this.price
+        }
+    }
+
+    private validateErrors() {
+        this.validate();
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.errors);
         }
     }
 }
